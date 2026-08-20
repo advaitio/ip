d15225d7b6c49baf330780cc0e6b95cfa0f913da
Advait Phadnis <advait.phadnis19@gmail.com>
2026-08-20 11:38:07 +0800

    Rename chatbot to Nudge and Update ASCII-art banner

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Nudge User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Nudge.java
@@ -0,0 +1,14 @@
+/**
+ * Starts the Nudge chatbot application.
+ */
+public class Nudge {
+    public static void main(String[] args) {
+        String banner = " _   _           _            \n"
+                + "| \\ | |_   _  __| | __ _  ___ \n"
+                + "|  \\| | | | |/ _` |/ _` |/ _ \\\n"
+                + "| |\\  | |_| | (_| | (_| |  __/\n"
+                + "|_| \\_|\\__,_|\\__,_|\\__, |\\___|\n"
+                + "                   |___/\n";
+        System.out.print(banner);
+    }
+}
